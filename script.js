cde357e6369ec5a47eb4dc897a95b25f171e039a
yuvrajthakur18 <ysthakur180401@gmail.com>
2023-03-19 23:25:25 +0530

    Added modal

---
+++ script.js
@@ -0,0 +1,19 @@
+function open_add_new() {
+    console.log (document.getElementById("buttons_right_side"));
+
+    document.getElementById("myModal").style.display="block";    
+}
+
+function closeX() {
+    console.log (document.getElementById("buttons_right_side"));
+
+    var myClose = document.getElementById("myModal").style.display="none";
+}
+
+/*
+var myClose = document.getElementsByClassName("close-x-mark");
+var modal = getElementById("myModal");
+
+myClose.onclick = function() {
+    "myModal".style.display="none";
+}*/
